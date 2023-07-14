38c4da5b7c076fab08be41533f3e60e73ff32d36
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-07-13 21:40:09 -0400

    started working on making the save button populate items

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,48 @@ const fetchMemes = () => {
 
 fetchGifs()
 fetchMemes()
+/*
+searchBtn = document.getElementById("searchBtn");
+//creating a variable so that we can alternate colors of the the recent search blocks
+let colorChoice = 1;
+//variable to determine which search we are on
+let searchNumber = 0
+
+//adds an event listener that gets the target of the event then goes and finds the texxt input from the child element
+searchBtn.addEventListener("click", function(event){
+    let z = event.target;
+    let zParent = z.parent.getAttribute("id");
+    //saves the text input? this is probably not correct
+    let input = zParent.userInput.text;
+    //saves the text to local storage with the search number as the key
+    localStorage.setItem(("" + searchNumber),("" + input));
+    //increases search number
+    searchNumber++;
+
+});
+
+*/
+
+/*
+//saves the div where the new buttons are going
+recentSearch = document.getElementById("recentSearch");
+*/
+
+/*
+//takes the last nine searches and appends them to the div
+ for (let i = searchNumber-9; i < searchNumber+1; i++){
+
+    recentSearch.innerHTML += "<button class = 'button" + colorChoice + "'>" + localStorage.getItem("" + i) + "</button>";
+
+    //once we make css for classes 1 2 and 3 this should set the background color to our three favorites and should alternate
+    if (colorChoice === 1){
+        colorChoice++
+    }else if (colorChoice === 2){
+        colorChoice ++
+    }else{
+        colorChoice = 1
+    }
+
+};
+
+*/
