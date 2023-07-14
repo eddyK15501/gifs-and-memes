9c813406a7fd5dcf1e4cc6faf084450b2e7a8972
michaelasivd <130368941+michaelasivd@users.noreply.github.com>
2023-07-13 21:52:11 -0400

    Navbar updated

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const giphyAPIKey = '6M9rze3zIiNSUB8y9OBLeDnETWFBztWy';
+const memesAPIKey = 'c2b7d1997fb54c2cad5ff44774377108';
+
+const fetchGifs = () => {
+    const requestURL = `https://api.giphy.com/v1/gifs/random?api_key=${giphyAPIKey}`
+
+    fetch(requestURL)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
+
+const fetchMemes = () => {
+    const requestURL = `https://api.humorapi.com/memes/random?api-key=${memesAPIKey}&media-type=image`
+
+    fetch (requestURL)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+fetchGifs()
+fetchMemes()
