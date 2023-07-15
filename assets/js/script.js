9fa666abe822f8133dcfd0ee04e907cd413a802e
eddyK15501 <itgma1231@gmail.com>
2023-07-14 21:37:37 -0400

    added comments in HTML, and modal function in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// API Keys
 const giphyAPIKey = '6M9rze3zIiNSUB8y9OBLeDnETWFBztWy';
 const memesAPIKey = 'c2b7d1997fb54c2cad5ff44774377108';
 
+// global variables
 let keyword = ''
 
 // ES6 syntax. fetchGifs function called with a parameter
@@ -12,13 +14,13 @@ const fetchGifs = (searchTerm) => {
         .then(data => {
             console.log(data)
 
-            // fetchMemes()
+            fetchMemes()
         })
 }
 
-// fetchMemes function called without a parameter. called with global variable: keyword
+// fetchMemes function called without a parameter. API endpoint query params called with global variable: keyword
 const fetchMemes = () => {
-    const requestURL = `https://api.humorapi.com/memes/search?api-key=${memesAPIKey}&keywords=${keyword}&media-type=image`
+    const requestURL = `https://api.humorapi.com/memes/search?api-key=${memesAPIKey}&keywords=${keyword}&media-type=image&number=10`
 
     fetch (requestURL)
         .then(res => res.json())
@@ -26,39 +28,43 @@ const fetchMemes = () => {
 }
 
 // ES5 syntax
+function openModal() {
+    $('.modal').addClass('is-active')
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+        
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+}
+
+// search a keyword in the input field, to fetch data from API
 function searchKeyword(event) {
-    // event.preventDefault(). submitting a form refreshes the page. stop the page from refreshing
+    // event.preventDefault(). submitting a form refreshes the page; stop the page from refreshing
     event.preventDefault()
-    //add search term to global variable: keyword
+    //store search term into global variable: keyword
     keyword = $('#userInput').val()
     // clear search input box
     $('#userInput').val('')
 
     console.log(keyword)
 
-    // if user typed in a search term, call fetchGifs of the keyword. else, pop up the modal to alert user to do so.
+    // if user typed in a search term, call fetchGifs with the keyword. else, pop up the modal to alert user to do so.
     if (keyword) {
         fetchGifs(keyword)
     } else {
-        $('.modal').addClass('is-active')
-
-        function closeModal($el) {
-            $el.classList.remove('is-active');
-        }
-
-        (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-            const $target = $close.closest('.modal');
-        
-            $close.addEventListener('click', () => {
-              closeModal($target);
-            });
-        });
-
+        openModal()
         return
     }
 }
 
 
 // addEventListener 
-// search for keyword, then click on the button, or press enter, to call searchKeyword function on submit
+// submit form to call searchKeyword function
 $('#searchForm').on('submit', searchKeyword)
