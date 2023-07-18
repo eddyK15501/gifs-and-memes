d7ecdf1dbd1b4dabf99235823149e43c75a64256
eddyK15501 <itgma1231@gmail.com>
2023-07-18 19:17:57 -0400

    tweek three 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ const fetchGifs = (searchTerm) => {
 
             let fetchedData = data.data
 
-            for (let i = 0; i < 15; i++) {
+            for (let i = 0; i < 10; i++) {
                 console.log(fetchedData[i].images.fixed_height.webp)
             }
 
@@ -63,9 +63,9 @@ function searchKeyword(event) {
     // event.preventDefault(). submitting a form refreshes the page; stop the page from refreshing
     event.preventDefault()
     //store search term into global variable: keyword
-    keyword = $('#userInput').val()
+    keyword = $('#user-input').val()
     // clear search input box
-    $('#userInput').val('')
+    $('#user-input').val('')
 
     console.log(keyword)
 
@@ -81,45 +81,46 @@ function searchKeyword(event) {
 
 // addEventListener 
 // submit form to call searchKeyword function
-$('#searchForm').on('submit', searchKeyword)
+$('#search-form').on('submit', searchKeyword)
 
 
-// searchForm = document.getElementById("searchForm");
-// //creating a variable so that we can alternate colors of the the recent search blocks
-// /*let colorChoice = 1;*/
-// //variable to determine which search we are on
-// let searchNumber = 0;
-// //adds an event listener that gets the target of the event then goes and finds the texxt input from the child element
-// searchForm.addEventListener("submit", function(event){
-//     let z = event.target;
-//     //let zParent = z.parentElement.getAttribute("id");
-//     //saves the text input? this is probably not correct
-//     let input = document.getElementById("userInput").value;
-//     //saves the text to local storage with the search number as the key
-//     localStorage.setItem(("" + searchNumber),("" + input));
-//     //increases search number
-//     searchNumber++;
-// });
+searchForm = document.getElementById("searchForm");
+//creating a variable so that we can alternate colors of the the recent search blocks
+/*let colorChoice = 1;*/
+//variable to determine which search we are on
+let searchNumber = 0;
+
+let keywordSearchArray = []
+//adds an event listener that gets the target of the event then goes and finds the texxt input from the child element
+searchForm.addEventListener("submit", function(event){
+    let z = event.target;
+    //let zParent = z.parentElement.getAttribute("id");
+    //saves the text input? this is probably not correct
+    let input = document.getElementById("userInput").value;
+    //saves the text to local storage with the search number as the key
+    localStorage.setItem(("" + searchNumber),("" + input));
+    //increases search number
+    searchNumber++;
+});
+
+//saves the div where the new buttons are going
+recentSearch = document.getElementById("recentSearch");
 
 
-// //saves the div where the new buttons are going
-// recentSearch = document.getElementById("recentSearch");
 
-
-
-// //takes the last nine searches and appends them to the div
-//  for (let i = 0; i < searchNumber +1; i++){
-//     if(localStorage.getItem(""+ i) === null){
-//         i++
-//     }else{
-//     recentSearch.innerHTML += "<button class = 'button'>" + localStorage.getItem("" + i) + "</button>";
-//     };
-//     //once we make css for classes 1 2 and 3 this should set the background color to our three favorites and should alternate
-//     /*if (colorChoice === 1){
-//         colorChoice++
-//     }else if (colorChoice === 2){
-//         colorChoice ++
-//     }else{
-//         colorChoice = 1
-//     }*/
-// };
+//takes the last nine searches and appends them to the div
+ for (let i = 0; i < searchNumber +1; i++){
+    if(localStorage.getItem(""+ i) === null){
+        i++
+    }else{
+    recentSearch.innerHTML += "<button class = 'button'>" + localStorage.getItem("" + i) + "</button>";
+    };
+    //once we make css for classes 1 2 and 3 this should set the background color to our three favorites and should alternate
+    /*if (colorChoice === 1){
+        colorChoice++
+    }else if (colorChoice === 2){
+        colorChoice ++
+    }else{
+        colorChoice = 1
+    }*/
+};
